f2ad726703f35d4e437f7c590fa60d1678930b80
Thanushan Pirapakaran <84732002+ThanushanP@users.noreply.github.com>
2022-10-13 21:54:14 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+//TO RUN: node server.js
+
+const Resume = require("./Resume");
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require('mongoose');
+
+let PORT = process.env.PORT || 3001;
+
+app.use (cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://User:data123@portfolio.fydnlbw.mongodb.net/Portfolio",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, function(err){
+  if (!err){
+    console.log("Database connected");
+  }
+  else {
+    console.log(err);
+  }
+})
+
+  
+app.get("/",(req,res) =>{
+  Resume.find({})
+    .then((items) =>res.json(items))
+    .catch((err) => console.log("error"));
+});
+
+app.listen(PORT, function(){
+  console.log("server is runnning");
+});
